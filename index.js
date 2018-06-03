dc8dae8fe5cce0b7a94aa08e3b8dd302a827a946
Mark Pothier <mark.d.pothier@gmail.com>
2018-06-03 19:51:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarge() {
-  return document.querySelector("div#nested .target");
+  return document.querySelector("#nested.target");
 }
 
 function increaseRankBy(n) {
