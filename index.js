628c77cf6162b015d0c0ad1b31453a5d559b65e9
Mark Pothier <mark.d.pothier@gmail.com>
2018-06-03 20:08:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   var lis = document.querySelectorAll(".ranked-list li")
   for (let i=0; i < lis.length; i++) {
-    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+    lis[i].innerText = parseInt(lis[i].innerText) + n
   }
 }
 
